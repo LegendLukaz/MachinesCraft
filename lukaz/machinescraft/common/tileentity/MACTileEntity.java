ea9a8961b3651e7b7dfb9a665bf6e02db8ad3c58
LegendLukaz <lukasfan08@gmail.com>
2014-02-06 15:48:22 +0800

    New: General tile entity file for mod's tile entities

---
+++ lukaz/machinescraft/common/tileentity/MACTileEntity.java
@@ -0,0 +1,20 @@
+package lukaz.machinescraft.common.tileentity;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public class MACTileEntity extends TileEntity {
+
+
+	 @Override
+	 public void func_145841_b(NBTTagCompound p_145841_1_)
+	   {
+	        super.func_145841_b(p_145841_1_);
+	   }
+
+	   @Override
+	   public void func_145839_a(NBTTagCompound p_145839_1_)
+	   {
+	        super.func_145839_a(p_145839_1_);
+	   }
+}
